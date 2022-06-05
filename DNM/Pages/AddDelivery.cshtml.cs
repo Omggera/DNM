95425f4c79db80bd6862e3c3c159183ce364d14f
Omggera <omggera@gmail.com>
2022-06-05 23:31:36 +0300

    добавил, тут, разного

--- DNM/Pages/AddDelivery.cshtml.cs
+++ DNM/Pages/AddDelivery.cshtml.cs
@@ -4,9 +4,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace DNM.Pages
 {
     public class AddDeliveryModel : PageModel
-    {       
-        public void OnGet()
+    {
+        public List<string> wbCode = new List<string>(); 
+        public void OnPost(string shtrih)
         {
+            wbCode.Add(shtrih);
         }
     }
 }
