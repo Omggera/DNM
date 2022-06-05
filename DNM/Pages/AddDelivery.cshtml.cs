bf866629aa7794d82565d072b707c5a92ba87360
Omggera <omggera@gmail.com>
2022-06-05 14:52:58 +0300

    Добавьте файлы проекта.

---
+++ DNM/Pages/AddDelivery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DNM.Pages
+{
+    public class AddDeliveryModel : PageModel
+    {       
+        public void OnGet()
+        {
+        }
+    }
+}
